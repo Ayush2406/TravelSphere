a775169a754ea69dcb99e8a585c231da71e85e2d
Ayush Gupta <dayush0524@gmail.com>
2026-09-15 01:03:42 +0530

    pages

--- code/backend/app/models/post.py
+++ code/backend/app/models/post.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import relationship
 
 from app.database import Base
+from app.models.destination import Destination
 
 
 class Post(Base):
