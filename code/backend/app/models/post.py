42893d0875908e6ea5c7c74b61420775c7546160
Ayush Gupta <dayush0524@gmail.com>
2026-09-15 01:32:02 +0530

    post

--- code/backend/app/models/post.py
+++ code/backend/app/models/post.py
@@ -4,10 +4,7 @@ from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import relationship
 
 from app.database import Base
-<<<<<<< HEAD
 from app.models.destination import Destination
-=======
->>>>>>> origin/niharika
 
 
 class Post(Base):
